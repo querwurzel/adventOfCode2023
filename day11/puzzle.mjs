b391ea3cc2a22a0e12d81a1ecb682aa0b82bef8b
querwurzel <>
2023-12-11 11:08:02 +0100

    day 11

---
+++ day11/puzzle.mjs
@@ -0,0 +1,78 @@
+import {input} from "./input.mjs";
+
+// n! / ((n - m)! * m!) = 36
+export const testInput = `
+...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....`; // 10x10
+
+function parse(input) {
+    let lines = input.split("\n").filter(line => !!line);
+    let galaxies = []
+
+    let id = 1;
+    for (let y = 0; y < lines.length; y++) {
+        for (let x = 0; x < lines[y].length; x++) {
+            if (lines[y][x] === "#") {
+                galaxies.push({
+                    x: x,
+                    y: y,
+                    id: id++,
+                    originalX: x,
+                    originalY: y
+                })
+            }
+        }
+    }
+
+    for (let idx = 0; idx< lines.length; idx++) {
+        if (!galaxies.find(gal => gal.originalY === idx)) { // empty column
+            galaxies
+                .filter(gal => gal.originalY >= idx)
+                .forEach(gal => gal.y++)
+        }
+        if (!galaxies.find(gal => gal.originalX === idx)) { // empty row
+            galaxies
+                .filter(gal => gal.originalX >= idx)
+                .forEach(gal => gal.x++)
+        }
+    }
+
+    console.log("Found galaxies", galaxies.length)
+    if (input === testInput) {
+        console.dir(galaxies)
+    }
+
+    return galaxies;
+}
+
+function solve(input) {
+    const galaxies = parse(input)
+
+    let total = 0, totalOps = 0;
+    for (let i = 0; i < galaxies.length; i++) {
+        let galaxy = galaxies[i];
+
+        for (let j = i + 1; j < galaxies.length; j++) {
+            let neighbor = galaxies[j];
+            let xDistance = Math.abs(galaxy.x - neighbor.x)
+            let yDistance = Math.abs(galaxy.y - neighbor.y)
+
+            //console.log(galaxy.id + " to " + neighbor.id + " is " + (xDistance + yDistance))
+
+            totalOps++
+            total += xDistance + yDistance
+        }
+    }
+
+    console.log("total distance:", total, totalOps)
+}
+
+solve(input)
